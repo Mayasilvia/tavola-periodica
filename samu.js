4c392d0d62515261702c68f04609c7813a7c3d00
Mayasilvia <silvia.paoli@iismontalegenova.it>
2024-11-19 12:39:39 +0100

    super tavola

--- samu.js
+++ samu.js
@@ -1,34 +1,38 @@
 
-let Idrogeno = {};
 
-Idrogeno.simbolo = 'H';
-Idrogeno.numero = 1;  
-Idrogeno.nome = 'Idrogeno';
 
-let elio = {};
 
-elio.simbolo = 'He';
-elio.numero = 2;
-elio.nome = 'Elio';
-
-let elementi = [Idrogeno, elio]
 for (const element of elementi) {
-    let cinesino = document.getElementById('cinesino');
+    let cinesino = document.getElementById('cinesino'); 
+    cinesino.style.position ='relative';
 
 
 let dallo = document.createElement('div');
 dallo.style.borderStyle = 'solid';
-dallo.style.width = '70px';
+dallo.style.borderWidth = '0.5px';
+dallo.style.width = '90px';
+dallo.style.height = '120px';
+dallo.style.position = 'absolute';
+dallo.style.left = element.xpos * 90 + 'px';
+dallo.style.top = element.ypos * 120 + 'px';
+dallo.style.backgroundColor = '#'+ element['cpk-hex']
+dallo.style.textAlign = 'center'
+dallo.style.boxSizing = 'border-box'
+
 let samuele = document.createElement('p');
-samuele.innerText = element.numero;
-dallo.appendChild(samuele);
+samuele.innerText = element.number;
+dallo.appendChild(samuele); 
 
 let balzino = document.createElement('a');
-balzino.innerText = element.simbolo ;
+balzino.innerText = element.symbol ;
+balzino.href=element.source
 dallo.appendChild(balzino);
+balzino.style.fontSize = '24px'
+balzino.style.fontWeight = 'bold';
+balzino.style.color = 'black'
 
 let cine =document.createElement('p');
-cine.innerText = element.nome;
+cine.innerText = element.name;
 dallo.appendChild(cine);
 cinesino.appendChild(dallo);
 
